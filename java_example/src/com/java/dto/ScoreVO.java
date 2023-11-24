d3bf101cf36d1388a35ca9436844275451df4ff4
Kimminjong <mkj425946@gmail.com>
2023-11-24 17:00:59 +0900

    11/24 Class문

--- java_example/src/com/java/dto/ScoreVO.java
+++ java_example/src/com/java/dto/ScoreVO.java
@@ -1,7 +1,11 @@
 package com.java.dto;
+
+import java.util.Scanner;
+
 //package가 다르면 앞에 설정을 해줘야함
 public class ScoreVO {
-	
+	//class Field에 있는 변수들은 알아서 초기화됨
+	//instance member - static X
     public String name;
     public int math;
     public int kor;
@@ -13,5 +17,34 @@ public class ScoreVO {
     }
     public float evg() {
     	return ((int)(total()/4f*100+0.5))/100f;
+    }
+    //void는 return type이 없을때,사용
+    public void input() {
+    	Scanner scann = new Scanner(System.in);
+		
+		System.out.print("이름 : ");
+		name = scann.nextLine();
+		
+		System.out.print("국어 : ");
+		kor = scann.nextInt();scann.nextLine();
+		
+		System.out.print("영어 : ");
+		eng = scann.nextInt();scann.nextLine();
+		
+		System.out.print("수학 : ");
+		math = scann.nextInt();scann.nextLine();
+		
+		System.out.print("과학 : "); 
+		scien = scann.nextInt();scann.nextLine();
+		
+		System.out.println();
+    }
+    //static을 붙여서 Class member
+    public static void printLabel() {
+    	System.out.println("순번\t이름\t국어\t영어\t수학\t과학\t총점\t평균");
+    }
+    public void printScore() {
+    	System.out.println(name+"\t"+kor+"\t"+eng+"\t"+math+"\t"+scien+"\t"+total()
+							+"\t"+evg());
     }
 }
---
+++ java_example/src/com/java/main/ScoreMG.java
@@ -0,0 +1,29 @@
+package com.java.main;
+
+import java.util.Scanner;
+
+import com.java.dto.ScoreVO;
+
+public class ScoreMG {
+
+	public static void main(String[] args) {
+		// 2번 문제에서 2차원 배열로 다시 설계
+		final int STUDEN_NUM;
+		
+		Scanner scann = new Scanner(System.in);
+
+		System.out.print("학생 수 : ");
+		STUDEN_NUM = scann.nextInt();scann.nextLine();
+		
+		ScoreVO[] scores = new ScoreVO[STUDEN_NUM];//address를 STUDEN_NUM크기 만큼 생성
+		//입력
+		for(int i =0; i<STUDEN_NUM;i++) {
+			scores[i] = new ScoreVO();
+			scores[i].input();
+		}		
+		
+		//출력
+//		new ScoreVO().printLabel(); instance를 선언해줘야만 실행가능
+		ScoreVO.printLabel();
+	}
+}
