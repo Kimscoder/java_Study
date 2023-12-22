f58b273e50e3ac905bcfd9933114441b6da0caf3
Kimscoder <124653142+Kimscoder@users.noreply.github.com>
2023-12-22 15:20:02 +0900

    12/22 jdbc Controller및 Main문 작성

--- java_jdbc/src/com/jdbc/dao/DAO.java
+++ java_jdbc/src/com/jdbc/dao/DAO.java
@@ -2,6 +2,8 @@ package com.jdbc.dao;
 
 import java.util.List;
 import com.jdbc.dto.VO;
+
+//기능서비스를 위한 인터페이스
 public interface DAO<E extends VO> {
 	List<E> selectList()throws Exception;
 	E selectById(String id)throws Exception;
--- java_jdbc/test/com/jdbc/test/Test고객DAO.java
+++ java_jdbc/test/com/jdbc/test/Test고객DAO.java
@@ -7,29 +7,47 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.jdbc.dao.고객DAO;
+import com.jdbc.dao.제품DAO;
+import com.jdbc.dao.주문DAO;
 import com.jdbc.datasource.DataSource;
 import com.jdbc.dto.고객VO;
+import com.jdbc.dto.제품VO;
+import com.jdbc.dto.주문VO;
+
 
 public class Test고객DAO {
 	
 	private DataSource dataSource;
 	private 고객DAO dao = new 고객DAO();//결합도 강하게
-	@Before
+	private 제품DAO dao1 = new 제품DAO();
+	private 주문DAO dao2 = new 주문DAO()
+;	@Before
 	public void init() {
 		dataSource = MocDataSource.getInstance();
 		dao.setDataSource(dataSource);
+		dao1.setDataSource(dataSource);
+		dao2.setDataSource(dataSource);
 	}
 		
+	
 	@Test
-	public void test1() throws Exception{
+	public void test고객리스트() throws Exception{
 		List<고객VO> 고객리스트 =dao.selectList();
 		
 		Assert.assertEquals(2,고객리스트.size());
 	}
+	@Test
+	
+	public void test제품리스트() throws Exception{
+		List<제품VO> 제품리스트 = dao1.selectList();
+		
+		Assert.assertEquals(7,제품리스트.size());
+	}
 	
 	@Test
-	public void test2() {}
-	
-	@Test
-	public void test3() {}
+	public void test주문리스트() throws Exception{
+		List<주문VO> 주문리스트 =  dao2.selectList();
+		
+		Assert.assertEquals(10, 주문리스트.size());
+	}
 }
